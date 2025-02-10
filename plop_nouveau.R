dd1d0f4b6366f936b25686169ebb24f8d1363282
vincent guyader <vincent@thinkr.fr>
2025-02-10 16:29:54 +0100

    Update plop_nouveau.R
    fix: correction d'une type depuis l'interface web

--- plop_nouveau.R
+++ plop_nouveau.R
@@ -2,6 +2,8 @@ gert::git_remote_list()
 gert::git_remote_remove(remote = "origin")
 gert::git_remote_add(url = "https://github.com/gitfaeg4/premierprojetvincent.git", name = "origin")
 
-1+1
+1+2
 5*5
 6565
+
+nopuveau message depuis github
